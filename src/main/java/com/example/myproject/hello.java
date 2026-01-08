dedd10a5a0c0487728aa25f084ab047ace28bfee
sudhiksha007 <sudhi.shan07@gmail.com>
2026-01-08 15:52:24 +0530

    Heello, this is my first commit!

---
+++ src/main/java/com/example/myproject/hello.java
@@ -0,0 +1,11 @@
+package com.example.myproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class hello {
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from MIT-FIS!";
+	}
+}
